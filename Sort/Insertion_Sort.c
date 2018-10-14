555589261028949808ff7063eecc4996544f8ded
Austin Benavidez <stnbnvdz@gmail.com>
2018-10-14 06:26:36 -0700

    Update Insertion_Sort.c

--- Sort/Insertion_Sort.c
+++ Sort/Insertion_Sort.c
@@ -5,18 +5,26 @@
 int main()
 {
   int n, array[1000], c, d, t;
+
+ /* Enter number of elements */
  
   printf("Enter number of elements\n");
   scanf("%d", &n);
  
   printf("Enter %d integers\n", n);
  
-  for (c = 0; c < n; c++)
+ /* Function to sort an array using insertion sort*/
+ 
+ for (c = 0; c < n; c++)
     scanf("%d", &array[c]);
  
   for (c = 1 ; c <= n - 1; c++) {
     d = c;
  
+   /* Move elements of arr[0..i-1], that are 
+          greater than key, to one position ahead 
+          of their current position */
+   
     while ( d > 0 && array[d-1] > array[d]) {
       t          = array[d];
       array[d]   = array[d-1];
@@ -25,6 +33,8 @@ int main()
       d--;
     }
   }
+ 
+// A utility function to print an array of size n 
  
   printf("Sorted list in ascending order:\n");
  
